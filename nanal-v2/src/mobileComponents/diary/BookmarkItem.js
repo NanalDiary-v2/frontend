8f2c5e1fdb7616b8de2a150c375f2ae571dc595c
recorror <itgwd9701@gmail.com>
2023-08-28 16:26:53 +0900

    refact : bookmark에서 안 쓰는 것들 삭제

--- nanal-v2/src/mobileComponents/diary/BookmarkItem.js
+++ nanal-v2/src/mobileComponents/diary/BookmarkItem.js
@@ -1,27 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function BookmarkItem({
-  diaryIdx,
-  diaryDate,
-  content,
-  emo,
-  nickname,
-  picture,
-}) {
+function BookmarkItem({ diaryIdx, diaryDate, content, picture }) {
   return (
     <Link
-      to={'/Diary/Detail'}
+      to={"/Diary/Detail"}
       state={{ diaryIdx: diaryIdx, diaryDate: diaryDate }}
     >
-      <div className='relative my-5 group'>
+      <div className="relative my-5 group">
         <img
           src={picture}
-          alt='DALL:E2'
-          className='absolute inset-0 object-cover p-1 rounded-lg group-hover:opacity-50'
+          alt="DALL:E2"
+          className="absolute inset-0 object-cover p-1 rounded-lg group-hover:opacity-50"
         />
-        <div className='transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0'>
-          <div className='p-2'>
-            <p className='text-sm truncate ... font-bold mt-10'>{content}</p>
+        <div className="transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
+          <div className="p-2">
+            <p className="text-sm truncate ... font-bold mt-10">{content}</p>
           </div>
         </div>
       </div>
--- nanal-v2/src/webComponents/diary/BookmarkItem.js
+++ nanal-v2/src/webComponents/diary/BookmarkItem.js
@@ -1,27 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function BookmarkItem({
-  diaryIdx,
-  diaryDate,
-  content,
-  emo,
-  nickname,
-  picture,
-}) {
+function BookmarkItem({ diaryIdx, diaryDate, content, picture }) {
   return (
     <Link
-      to={'/Diary/Detail'}
+      to={"/Diary/Detail"}
       state={{ diaryIdx: diaryIdx, diaryDate: diaryDate }}
     >
-      <div className='relative group'>
+      <div className="relative group">
         <img
           src={picture}
-          alt='DALL:E2'
-          className='absolute inset-y-8 object-cover p-1 rounded-lg group-hover:opacity-50'
+          alt="DALL:E2"
+          className="absolute inset-y-8 object-cover p-1 rounded-lg group-hover:opacity-50"
         />
-        <div className='transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0'>
-          <div className='p-2'>
-            <p className='text-sm truncate ... font-bold mt-10'>{content}</p>
+        <div className="transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
+          <div className="p-2">
+            <p className="text-sm truncate ... font-bold mt-10">{content}</p>
           </div>
         </div>
       </div>
