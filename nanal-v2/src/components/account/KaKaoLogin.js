0db7aa8874d77736760e86679fb6db23f6464e4b
soyeon_9505 <soyeon_9605@yu.ac.kr>
2023-08-29 21:29:15 +0900

    refator : kakaoLogin Component

--- nanal-v2/src/components/account/KaKaoLogin.js
+++ nanal-v2/src/components/account/KaKaoLogin.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { Link, useNavigate } from 'react-router-dom';
+import axios_api from '../../config/Axios';
 import { setCookie } from '../../config/Cookie';
+import { onLogin } from '../../config/Login';
 
 function KaKaoLogin() {
   const navigate = useNavigate();
@@ -22,13 +23,48 @@ function KaKaoLogin() {
       sameSite: 'none',
     });
 
-    // console.log('토큰 파싱' + token);
-    // console.log(token);
-
     if (token) {
       window.location.replace('/home');
     } else {
-      window.location.replace('/');
+      if (accessToken !== undefined) {
+        onLogin();
+        axios_api
+          .get('user/profile')
+          .then(({ data }) => {
+            if (data.statusCode === 200) {
+              if (data.data.responseMessage === '회원 정보 조회 성공') {
+                // console.log(data.data.profile);
+                window.localStorage.setItem(
+                  'profileDays',
+                  data.data.profile.days
+                );
+                window.localStorage.setItem(
+                  'profileImg',
+                  data.data.profile.img
+                );
+                window.localStorage.setItem(
+                  'profileIntroduction',
+                  data.data.profile.introduction
+                );
+                window.localStorage.setItem(
+                  'profileNickname',
+                  data.data.profile.nickname
+                );
+                navigate(`/home`, {
+                  replace: true,
+                });
+                window.location.reload();
+              }
+            } else {
+              console.log('회원 정보 오류: ');
+              console.log(data.statusCode);
+              console.log(data.data.responseMessage);
+            }
+          })
+          .catch(({ error }) => {
+            console.log('회원 정보 조회: ' + error);
+          });
+      }
     }
   }, []);
 }
