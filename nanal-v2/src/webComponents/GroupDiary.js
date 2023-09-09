d188172c73b19aa6ba21dee4854e956052fbf4d7
recorror <itgwd9701@gmail.com>
2023-09-09 16:43:04 +0900

    refact : groupCreate 합침.

--- nanal-v2/src/webComponents/GroupDiary.js
+++ nanal-v2/src/webComponents/GroupDiary.js
@@ -2,29 +2,20 @@ import nmo from '../src_assets/img/bookmark-name/name-mark-orange.svg';
 import diaryImgOrange from '../src_assets/img/diary-img/diary-img-orange.svg';
 import React, { useState } from 'react';
 import GroupList from './group/GroupList';
-import GroupCreate from './group/GroupCreate';
+import GroupCreate from '../components/group/GroupCreate';
 import GroupDetail from './group/GroupDetail';
 import GroupSetting from './group/GroupSetting';
 import GroupUpdate from './group/GroupUpdate';
 
 const GroupDiary = () => {
-  const [groupCompo, setGroupCompo] = useState([
-    true,
-    false,
-    false,
-    false,
-    false,
-  ]);
+  const [groupCompo, setGroupCompo] = useState([true, false, false, false, false]);
   const [groupIdx, setGroupIdx] = useState();
 
   return (
     <div className='absolute w-[1440px] mx-auto'>
       <p className='absolute z-30 left-[330px] inset-y-36'>그룹일기</p>
       <img src={nmo} className='absolute z-20 left-60 inset-y-28' />
-      <img
-        src={diaryImgOrange}
-        className='absolute w-[1280px] z-10 left-12 top-8'
-      />
+      <img src={diaryImgOrange} className='absolute w-[1280px] z-10 left-12 top-8' />
       <div className='absolute z-20 left-36 inset-y-[216px]'>
         <GroupList setGroupCompo={setGroupCompo} setGroupIdx={setGroupIdx} />
       </div>
@@ -35,10 +26,7 @@ const GroupDiary = () => {
         </div>
       ) : groupCompo[1] === true ? (
         <div className='absolute z-20 right-80 inset-y-[120px] w-[400px]'>
-          <GroupCreate
-            setGroupCompo={setGroupCompo}
-            setGroupIdx={setGroupIdx}
-          />
+          <GroupCreate setGroupCompo={setGroupCompo} setGroupIdx={setGroupIdx} />
         </div>
       ) : groupCompo[2] === true ? (
         <div className='absolute z-20 right-80 inset-y-52 w-[400px]'>
