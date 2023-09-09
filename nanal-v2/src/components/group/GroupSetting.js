eb9f2d72ebb0a8945acd8f0102235e1978d8217b
recorror <itgwd9701@gmail.com>
2023-09-09 21:20:00 +0900

    refact : group diary reduce

--- nanal-v2/src/components/group/GroupSetting.js
+++ nanal-v2/src/components/group/GroupSetting.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-// import { useLocation } from 'react-router';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import axios_api from '../../config/Axios';
 import { onLogin } from '../../config/Login';
 import FriendItem from '../friend/FriendItem';
+import { BrowserView, MobileView, isMobile } from 'react-device-detect';
 
-function GroupSetting() {
+function GroupSetting({ groupIdx, setGroupCompo }) {
+  const mobile = isMobile;
+
   const { state } = useLocation();
   const navigate = useNavigate();
 
@@ -34,11 +36,24 @@ function GroupSetting() {
       if (result.isConfirmed) {
         // 확인(예) 버튼 클릭 시 이벤트
         axios_api
-          .delete(`group/${state.groupIdx}`)
+          .delete(`group/${mobile ? state.groupIdx : groupIdx}`)
           .then(({ data }) => {
             if (data.statusCode === 200) {
               if (data.data.responseMessage === '그룹 탈퇴 성공') {
-                navigate('/Group/List');
+                mobile
+                  ? navigate('/Group/List')
+                  : Swal.fire({
+                      title: '그룹 나가기',
+                      text: '그룹을 탈퇴하셨습니다.',
+                      icon: 'success',
+                      confirmButtonColor: '#0891b2',
+                      confirmButtonText: '확인',
+                      width: '30%',
+                    }).then(() => {
+                      navigate('/home');
+                      window.location.reload();
+                      // reload 해줘야 한당...
+                    });
               }
             } else {
               console.log('그룹 탈퇴 오류: ');
@@ -98,60 +113,108 @@ function GroupSetting() {
   }, []);
 
   return (
-    <div className='justify-center text-center'>
-      {/* <h1 className='text-center'>그룹 상세 페이지</h1> */}
+    <>
+      <MobileView>
+        <div className='justify-center text-center'>
+          {/* <h1 className='text-center'>그룹 상세 페이지</h1> */}
 
-      <div className='justify-center w-full my-2 text-center'>
-        <div className=''>
-          <img
-            src={groupDetail.imgUrl}
-            className='inline-block p-1 rounded-md w-28 h-28'
-          ></img>
-          <p className='inline-block ml-5 text-2xl font-bold text-center'>
-            {groupDetail.groupName}
-          </p>
-        </div>
-        <div className='my-1 mb-2 break-words'>
-          {groupTag.map((tagging, idx) => {
-            if (tagging.tag)
-              return (
-                <span
-                  key={idx}
-                  className='items-center inline-block p-1 mx-2 my-1 text-xs break-all rounded-lg bg-slate-200 hover:bg-blue-300'
-                >
-                  #{tagging.tag}
-                </span>
-              );
-          })}
-        </div>
-        <div>
-          <Link
-            to={`/Group/Update`}
-            state={{ groupDetail: groupDetail.groupIdx }}
-          >
-            <button className='bg-cyan-600 text-white px-2.5 py-1 rounded-3xl m-auto mx-10 inline-block'>
-              수정하기
-            </button>
-          </Link>
-          <button
-            type='button'
-            className='bg-rose-600 text-white px-2.5 py-1 rounded-3xl m-auto mx-10 inline-block'
-            onClick={deleteGroup}
-          >
-            탈퇴하기
-          </button>
-        </div>
-        <hr className='mx-auto my-5 border-solid border-1 border-slate-800 w-80' />
-      </div>
+          <div className='justify-center w-full my-2 text-center'>
+            <div className=''>
+              <img src={groupDetail.imgUrl} className='inline-block p-1 rounded-md w-28 h-28'></img>
+              <p className='inline-block ml-5 text-2xl font-bold text-center'>
+                {groupDetail.groupName}
+              </p>
+            </div>
+            <div className='my-1 mb-2 break-words'>
+              {groupTag.map((tagging, idx) => {
+                if (tagging.tag)
+                  return (
+                    <span
+                      key={idx}
+                      className='items-center inline-block p-1 mx-2 my-1 text-xs break-all rounded-lg bg-slate-200 hover:bg-blue-300'
+                    >
+                      #{tagging.tag}
+                    </span>
+                  );
+              })}
+            </div>
+            <div>
+              <Link to={`/Group/Update`} state={{ groupDetail: groupDetail.groupIdx }}>
+                <button className='bg-cyan-600 text-white px-2.5 py-1 rounded-3xl m-auto mx-10 inline-block'>
+                  수정하기
+                </button>
+              </Link>
+              <button
+                type='button'
+                className='bg-rose-600 text-white px-2.5 py-1 rounded-3xl m-auto mx-10 inline-block'
+                onClick={deleteGroup}
+              >
+                탈퇴하기
+              </button>
+            </div>
+            <hr className='mx-auto my-5 border-solid border-1 border-slate-800 w-80' />
+          </div>
 
-      {friendList[0] === '아직은 친구가 없습니다.' ? (
-        <p>아직은 그룹에 친구가 없습니다.</p>
-      ) : (
-        friendList.map((friendItem, idx) => (
-          <FriendItem key={idx} item={friendItem} />
-        ))
-      )}
-    </div>
+          {friendList[0] === '아직은 친구가 없습니다.' ? (
+            <p>아직은 그룹에 친구가 없습니다.</p>
+          ) : (
+            friendList.map((friendItem, idx) => <FriendItem key={idx} item={friendItem} />)
+          )}
+        </div>
+      </MobileView>
+      <BrowserView>
+        <div className='justify-center text-center w-[500px] h-[420px] overflow-auto pr-2'>
+          {/* <h1 className='text-center'>그룹 상세 페이지</h1> */}
+
+          <div className='justify-center w-full my-2 text-center'>
+            <div className='my-3'>
+              <img src={groupDetail.imgUrl} className='inline-block p-1 rounded-md w-28 h-28'></img>
+              <p className='inline-block ml-5 text-2xl font-bold text-center'>
+                {groupDetail.groupName}
+              </p>
+            </div>
+            <div className='my-1 mb-2 break-words'>
+              {groupTag.map((tagging, idx) => {
+                return (
+                  <span
+                    key={idx}
+                    className='items-center inline-block p-1 mx-2 my-1 text-xs break-all rounded-lg bg-slate-200 hover:bg-blue-300'
+                  >
+                    #{tagging.tag}
+                  </span>
+                );
+              })}
+            </div>
+            <div className='flex justify-center'>
+              <div onClick={() => setGroupCompo([false, false, false, false, true])}>
+                <button className='inline-block px-2 py-1 m-auto mx-5 text-white bg-cyan-600 rounded-3xl'>
+                  수정하기
+                </button>
+              </div>
+              <button
+                type='button'
+                className='inline-block px-2 py-1 m-auto mx-5 text-white bg-rose-600 rounded-3xl'
+                onClick={deleteGroup}
+              >
+                탈퇴하기
+              </button>
+            </div>
+            <hr className='mx-auto my-5 border-solid border-1 border-slate-800 w-80' />
+          </div>
+
+          {friendList[0] === '아직은 친구가 없습니다.' ? (
+            <p>아직은 그룹에 친구가 없습니다.</p>
+          ) : (
+            <div>
+              <p className='pb-2'>그룹에 가입된 친구 목록 입니다.</p>
+              {friendList.map((friendItem, idx) => (
+                <FriendItem key={idx} item={friendItem} />
+              ))}
+            </div>
+          )}
+        </div>
+      </BrowserView>
+    </>
   );
 }
 
